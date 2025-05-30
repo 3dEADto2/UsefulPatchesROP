0b321a07acca35112fca6554c22db870ce621c80
3dEADto2 <henriknoeckel@gmail.com>
2025-05-30 09:03:18 +0200

    fix for tallgrassspawnpatch

--- Patches/FieldPatch.cs
+++ Patches/FieldPatch.cs
@@ -7,11 +7,18 @@ namespace UsefullPatches.Patches
     internal class FieldSpawnResourcePatch
     {
         [HarmonyPrefix]
-        public static bool Prefix()
+        public static bool Prefix(ref HittableResource resource)
         {
+            if (!ConfigManager.FieldSpawnTallGrass!.Value && resource.Name == "Generic weed" && resource.DontCollide == true)
+            {
+                if (ConfigManager.EnableLogging!.Value) {
+                    UsefullPatchesMain.Log?.LogInfo("Field: ResourceSpawn TallGrass spawned!");
+                }
+                return true;
+            }
             if (ConfigManager.EnableLogging!.Value)
             {
-                UsefullPatchesMain.Log?.LogInfo("Field: ResourceSpawn denied!");
+                UsefullPatchesMain.Log?.LogInfo("Field: ResourceSpawn " + resource.Name + " denied!");
             }
             return false;
         }
@@ -21,8 +28,9 @@ namespace UsefullPatches.Patches
     internal class FieldSpawnTreePatch
     {
         [HarmonyPrefix]
-        public static bool Prefix()
+        public static bool Prefix(ref bool __result)
         {
+            __result = false;
             if (ConfigManager.EnableLogging!.Value)
             {
                 UsefullPatchesMain.Log?.LogInfo("Field: TreeSpawn denied!");
@@ -35,8 +43,9 @@ namespace UsefullPatches.Patches
     internal class FieldSpawnLargeResourcePatch
     {
         [HarmonyPrefix]
-        public static bool Prefix()
+        public static bool Prefix(ref bool __result)
         {
+            __result = false;
             if (ConfigManager.EnableLogging!.Value)
             {
                 UsefullPatchesMain.Log?.LogInfo("Field: LargeResourceSpawn denied!");
--- plugin.cs
+++ plugin.cs
@@ -15,7 +15,7 @@ namespace UsefullPatches
     {
         private const string pluginGUID = "3dEADto.UsefullPatches";
         private const string pluginName = "Usefull Patches";
-        private const string pluginVersion = "1.2.1";
+        private const string pluginVersion = "1.2.2";
         private readonly Harmony harmony = new Harmony(pluginGUID);
 
         private static UsefullPatchesMain? Instance;
